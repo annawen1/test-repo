d49a5628c67a46b68aca2e66a28444fbc4596696
Anna Wen <54281166+annawen1@users.noreply.github.com>
2024-03-08 16:53:38 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ const Home = () => (
       type="centered"
       theme="g100"
       title="Lead space title"
-      copy="Use this area for a short line of copy to support the title here"
+      copy="Use this area for a short line of copy to support the title here hello"
       gradient={true}
       // buttons={buttons}
       image={{
